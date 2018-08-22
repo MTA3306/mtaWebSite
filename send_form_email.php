f8f03aedce0cb6857270a0c9329570f1f38ca16a
MTA3306 <40899547+MTA3306@users.noreply.github.com>
2018-08-22 09:16:52 -0400

    Update send_form_email.php

--- send_form_email.php
+++ send_form_email.php
@@ -3,32 +3,34 @@ if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "xen44324434@gmail.com";
-    $email_subject = "Bus Previously Purchase";
+    $email_subject = "you was mothafucking right gif";
  
     function died($error) {
         // your error code can go here
-        echo "We are very sorry, but an error was found in form you submitted. ";
-        echo "Aforementioned errors appear below.<br /><br />";
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
         echo $error."<br /><br />";
-        echo "Please go back and fix these errors before submitting your form again.<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
         die();
     }
  
  
     // validation expected data exists
-    if(!isset($_POST['name']) ||
+    if(!isset($_POST['first_name']) ||
+        !isset($_POST['last_name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['order_number']) ||
-        !isset($_POST['message'])) {
+        !isset($_POST['telephone']) ||
+        !isset($_POST['comments'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
  
      
  
-    $first_name = $_POST['name']; // required
+    $first_name = $_POST['first_name']; // required
+    $last_name = $_POST['last_name']; // required
     $email_from = $_POST['email']; // required
-    $telephone = $_POST['order_number']; // not required
-    $comments = $_POST['message']; // required
+    $telephone = $_POST['telephone']; // not required
+    $comments = $_POST['comments']; // required
  
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -40,11 +42,15 @@ if(isset($_POST['email'])) {
     $string_exp = "/^[A-Za-z .'-]+$/";
  
   if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  }
+ 
+  if(!preg_match($string_exp,$last_name)) {
+    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
   }
  
   if(strlen($comments) < 2) {
-    $error_message .= 'The message you entered does not appear to be valid.<br />';
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
   }
  
   if(strlen($error_message) > 0) {
@@ -61,10 +67,11 @@ if(isset($_POST['email'])) {
  
      
  
-    $email_message .= "Name: ".clean_string($first_name)."\n";
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Last Name: ".clean_string($last_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Order Number: ".clean_string($Order Number)."\n";
-    $email_message .= "Message: ".clean_string($message)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
  
 // create email headers
 $headers = 'From: '.$email_from."\r\n".
